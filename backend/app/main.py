58af0df9c9653e3bbd32dd473ff682aa000b64d4
Mohd Rizwan <mr659612@gmail.com>
2026-09-25 00:01:03 +0530

    Completed pdf/image to text conversion(level 1) using regex and llm

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI, UploadFile, File
+import os
+from fastapi import FastAPI, UploadFile, File, HTTPException
+
+from app.services.ocr_service import extract_text_from_image
+from app.services.pdf_service import extract_text_from_pdf
+from app.services.extraction_service import extract_basic_fields
 
 app  = FastAPI(title="Swasthya Doc AI")
 
@@ -10,13 +15,25 @@ def root():
 #In Python, ... is a built-in singleton object called Ellipsis
 #FastAPI and Pydantic use ... to mean: "This field is required, and there is no default value."
 async def upload_file(file: UploadFile = File(...)):
+    os.makedirs("uploads", exist_ok=True)
     file_path = f"uploads/{file.filename}"
 
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
 
-    return{
+    # Branch by file type to extract text
+    if file.filename.lower().endswith(".pdf"):
+        ocr_text = extract_text_from_pdf(file_path)
+    elif file.filename.lower().endswith((".png", ".jpg", ".jpeg")):
+        ocr_text = extract_text_from_image(file_path)
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="Unsupported file format. Please upload PDF, PNG, or JPG."
+        )
+    # Run extraction (Regex + ONNX LLM)
+    prescription_data = extract_basic_fields(ocr_text)
+    return {
         "filename": file.filename,
-        "content_type": file.content_type,
-        "message": "File uploaded successfully"
+        "data": prescription_data
     }
